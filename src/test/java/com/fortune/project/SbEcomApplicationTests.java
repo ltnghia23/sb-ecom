2489f960280371684934f3657245790e0d6d7b61
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-06-17 14:27:30 +0700

    Initial commit

---
+++ src/test/java/com/fortune/project/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortune.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
